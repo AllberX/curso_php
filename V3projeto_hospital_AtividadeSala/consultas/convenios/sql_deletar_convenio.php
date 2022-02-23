753354d58723f5ca4ac39a2bafc669d9003998b3
Alberto Castro <allber.analista@gmail.com>
2022-02-23 16:02:57 -0300

    V3projeto_hospital_AtividadeSala.php
    V3projeto_hospital_AtividadeSala

---
+++ V3projeto_hospital_AtividadeSala/consultas/convenios/sql_deletar_convenio.php
@@ -0,0 +1,37 @@
+<?php
+
+    //INICIANDO SESSAO
+    session_start();
+
+    //CHAMANDO ARQUIVO CONEXAO
+    include '../../conexao.php';
+
+    //CADA ../ SIGNIFICA VOLTAR UMA PASTA PARA TRAS   
+
+    //BUSCANDO DADOS DO CADASTRO DE CONVENIO via _POST
+    $var_cd_convenio = $_POST['frm_cd_convenio'];    
+ 
+
+    //PRIMEIRO PASSO PARA DELETAR DADOS
+    $insere_convenios = "DELETE FROM hospital.CONVENIO 
+    WHERE cd_convenio = '$var_cd_convenio'";
+
+    //SEGUNDO PASSO
+    //JUNTAR AS INFORMACOES DA CONEXAO + A CONSULTA
+    $valida_cadastro_convenio = mysqli_query($conexao, $insere_convenios);
+    //OBS a variavel $conexao é a mesma do conexao.php
+
+    //VALIDANDO SE OS DADOS FORAM DELETADOS
+    if(!$valida_cadastro_convenio){
+        //CRIANDO SESSAO MENSAGEM
+        $_SESSION['msg_execucao'] = 'Erro ao deletar o convênio.';        
+        header('Location: ../../convenios.php');
+    }else{
+        //CRIANDO SESSAO MENSAGEM
+        $_SESSION['msg_execucao'] = 'Convênio excluído com sucesso!';   
+        header('Location: ../../convenios.php');
+    }
+
+
+
+?>
